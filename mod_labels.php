876f40526b8d310836eb3d39de3cb7b9f53d17fc
David Shannon <dzshanno@yahoo.com>
2018-08-16 14:44:07 +0100

    Initial test commit

--- mod_labels.php
+++ mod_labels.php
@@ -6,12 +6,16 @@
  * @copyright  A copyright
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  * @link       http://your.url.com
+ *
+ *             Provide the ability to print to Avery Labels
  */
+ /**  use Joomla\CMS\Helper\ModuleHelper; */
 
-use Joomla\CMS\Helper\ModuleHelper;
+require_once dirname(__FILE__) . '/helper.php';
 
 defined('_JEXEC') or die;
 
-$moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
+// $moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
 
-require ModuleHelper::getLayoutPath('mod_labels', $params->get('layout', 'default'));
+$labeltemplate = modLabelsHelper::getLabelTemplate($params);
+require JModuleHelper::getLayoutPath('mod_labels', $params->get('layout', 'default'));
--- tmpl/default.php
+++ tmpl/default.php
@@ -10,10 +10,6 @@
 
 defined('_JEXEC') or die;
 
-// Access to module parameters
-$domain = $params->get('domain', 'https://www.joomla.org');
 ?>
 
-<a href="<?php echo $domain; ?>">
 	<?php echo 'labels'; ?>
-</a>
