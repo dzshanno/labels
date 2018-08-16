e3cdfe84d32cf1e94ff8447794d8e6444b60910b
David Shannon <dzshanno@yahoo.com>
2018-08-16 15:34:48 +0100

    updated hint

--- mod_labels.php
+++ mod_labels.php
@@ -15,7 +15,7 @@ require_once dirname(__FILE__) . '/helper.php';
 
 defined('_JEXEC') or die;
 
-// $moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
+$moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
 
 $labeltemplate = modLabelsHelper::getLabelTemplate($params);
 require JModuleHelper::getLayoutPath('mod_labels', $params->get('layout', 'default'));
