ebe2db3d7735d83d7c4af55c3e4af99e89c7b4be
David Shannon <dzshanno@yahoo.com>
2018-08-16 13:30:10 +0100

    Initial commit

---
+++ mod_labels.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package    labels
+ *
+ * @author     David <your@email.com>
+ * @copyright  A copyright
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * @link       http://your.url.com
+ */
+
+use Joomla\CMS\Helper\ModuleHelper;
+
+defined('_JEXEC') or die;
+
+$moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
+
+require ModuleHelper::getLayoutPath('mod_labels', $params->get('layout', 'default'));
