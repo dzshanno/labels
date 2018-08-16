265b6a622f006e2cbfec837b8ea7e106e62f9d23
David Shannon <dzshanno@yahoo.com>
2018-08-16 15:47:51 +0100

    bug fixing

--- mod_labels.php
+++ mod_labels.php
@@ -9,13 +9,10 @@
  *
  *             Provide the ability to print to Avery Labels
  */
- /**  use Joomla\CMS\Helper\ModuleHelper; */
 
 require_once dirname(__FILE__) . '/helper.php';
 
 defined('_JEXEC') or die;
-
-$moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
 
 $labeltemplate = modLabelsHelper::getLabelTemplate($params);
 require JModuleHelper::getLayoutPath('mod_labels', $params->get('layout', 'default'));
